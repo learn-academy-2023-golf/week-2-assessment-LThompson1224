d5da9c505aef8ab688433321c61d60e936e612f1
Ledarrien <ledarrienthompson@gmail.com>
2023-10-17 07:17:22 -0700

    adds week 2 assessment answers

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -8,34 +8,86 @@
 
 // Reminder: The test will call your function
 // Run the file with the following command: $ yarn jest
-
 // --------------------INSTRUCTOR EXAMPLE: Create a function that takes in an array of numbers and returns an array with all the numbers multiplied by 3.
 
 // a) Create a test with expect statements for each of the variables provided.
 
-const numbersArray1 = [6, 7, 8, 9, 10]
-// Expected output: [18, 21, 24, 27, 30]
-const numbersArray2 = [24, 27, 30, 33, 36]
-// Expected output: [72, 81, 90, 99, 108]
+// describe("multiplyBy3", () => {
+//   it("returns an array with all the numbers multiplied by 3.", () => {
+//     expect(multiplyBy3(numbersArray1)).toEqual([18,21,24]) expect(multiplyBy3(numbersArray2)).toEqual([72,81,90,99,108])
+//     ("Hello, LEARN student!")
+//   })
+// })
+// const numbersArray1 = [6, 7, 8, 9, 10]
+// // Expected output: [18, 21, 24, 27, 30]
+// const numbersArray2 = [24, 27, 30, 33, 36]
+// // Expected output: [72, 81, 90, 99, 108]
+
+// // b) Create the function that makes the test pass.
+
+// // Pseudo code:
+
+// // --------------------1) Create a function that takes a object as an argument and decides if the number inside it is evenly divisible by three or not.
+
+// // a) Create a test with expect statements for each of the variables provided.
+// // Function to check if a number is evenly divisible by three
+// function isDivisibleByThree(object) {
+//     const number = object.number;
+//     if (number % 3 === 0) {
+//       return `${number} is divisible by three`;
+//     } else {
+//       return `${number} is not divisible by three`;
+//     }
+//   }
+  
+//   // Test cases using Jest
+//   test('Check if a number is evenly divisible by three', () => {
+//     const object1 = { number: 15 };
+//     expect(isDivisibleByThree(object1)).toBe('15 is divisible by three');
+  
+//     const object2 = { number: 0 };
+//     expect(isDivisibleByThree(object2)).toBe('0 is divisible by three');
+  
+//     const object3 = { number: -7 };
+//     expect(isDivisibleByThree(object3)).toBe('-7 is not divisible by three');
+//   });
+  
+// const object1 = { number: 15 }
+// // Expected output: "15 is divisible by three"
+// const object2 = { number: 0 }
+// // Expected output: "0 is divisible by three"
+// const object3 = { number: -7 }
+// // Expected output: "-7 is not divisible by three"
 
 // b) Create the function that makes the test pass.
 
-// Pseudo code:
+// function isDivisibleByThree(object) {
+//     const number = object.number;
+//     if (typeof number !== 'number') {
+//       return 'Invalid input: Not a number';
+//     }
+  
+//     if (number % 3 === 0) {
+//       return `${number} is divisible by three`;
+//     } else {
+//       return `${number} is not divisible by three`;
+//     }
+//   }
+  
 
-// --------------------1) Create a function that takes a object as an argument and decides if the number inside it is evenly divisible by three or not.
+// Pseudo code:FUNCTION isDivisibleByThree(object):
+//create a failing test witha good fail 
+//once we get a good fail then we run a test that will pass 
+// number = object.number
 
-// a) Create a test with expect statements for each of the variables provided.
+//IF number is not a number:
+//RETURN "Invalid input"
+    
+//IF number is divisible by 3:
+// RETURN "Divisible by three"
+//ELSE:
+//RETURN "Not divisible by three"
 
-const object1 = { number: 15 }
-// Expected output: "15 is divisible by three"
-const object2 = { number: 0 }
-// Expected output: "0 is divisible by three"
-const object3 = { number: -7 }
-// Expected output: "-7 is not divisible by three"
-
-// b) Create the function that makes the test pass.
-
-// Pseudo code:
 
 // --------------------2) Create a function that takes in an array of words and returns an array with all the words capitalized.
 
@@ -46,6 +98,11 @@ const randomNouns1 = ["streetlamp", "potato", "teeth", "conclusion", "nephew"]
 const randomNouns2 = ["temperature", "database", "chopsticks", "mango"]
 // Expected output: ["Temperature", "Database", "Chopsticks", "Mango"]
 
+
+
 // b) Create the function that makes the test pass.
 
-// Pseudo code:
+// Pseudo code: 
+
+
+  
--- interview-questions.md
+++ interview-questions.md
@@ -10,25 +10,25 @@ Type your initials here to confirm you read the instructions:
 
 1. What is the difference between a parameter and an argument?
 
-Your answer:
+Your answer: a parameter to me is a variable in function. while the argument represents the value of the expression.
 
-Researched answer:
+Researched answer: Parameter: A parameter is a variable in a function or method declaration.That is a placeholder for data that will be passed into the function. when its called. Argument : an argument,on the other hand , is the actual value or expression that is passed into a funtion when its called. 
 
 2. What is the difference between an object and a class?
 
-Your answer:
+Your answer: classes are a template or a blueprint for objects.Objects a datatype that is a collection of things.
 
-Researched answer:
+Researched answer: a object is an istance of a class. based on the class blueprint.a class  is a blueprint or template for creating objects.
 
 3. What is iteration?
 
-Your answer:
+Your answer: repeating statements for a amount of time.
 
-Researched answer:
+Researched answer: the process of repeatedly executing a set of statements or a block of code. its a fundamental concept in programming and is often used to perform tasks multiple times.
 
 4. STRETCH: What is hoisting in JavaScript?
 
-Researched answer:
+Researched answer: hoisting in javascript where variable and function declarations are moved to the top of their containing scope during the compilation phrase.
 
 ## Looking Ahead: Terms for Next Week
 
